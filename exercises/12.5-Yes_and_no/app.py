1838fc0d08f716c3e0e1be04b25f1c8362f43ae7
Alvaro Chirinos <65077832+alvaro-chiri@users.noreply.github.com>
2023-10-12 15:45:24 +0000

    12.5 done

--- exercises/12.5-Yes_and_no/app.py
+++ exercises/12.5-Yes_and_no/app.py
@@ -1,5 +1,14 @@
 theBools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1]
 
 #Your code go here:
+def wikiwoko(x):
+    if x == 1:
+        return 'wiki'
+    else:
+        return 'woko'
+    
+new_list = list(map(wikiwoko, theBools))
+
+print(new_list)
 
 
