aa368d89aa6a59e4d0642255b6fe103da958aef8
Alvaro Chirinos <65077832+alvaro-chiri@users.noreply.github.com>
2023-10-12 20:31:16 +0000

    FINISHED EVERYTHING

--- exercises/16-Techno_beat/app.py
+++ exercises/16-Techno_beat/app.py
@@ -1,4 +1,21 @@
+def lyrics_generator(arr):
+    count = 0
+    lyrics = ''
 
+    for i in range(len(arr)):
+        if arr[i] == 0:
+            lyrics += 'Boom '
+            count = 0
+        if arr[i] == 1:
+            if count == 2:
+                lyrics += "Drop the base "
+                lyrics += "!!!Break the base!!! "
+                count = 0
+            else:
+                lyrics += "Drop the base "
+                count += 1
+    
+    return lyrics
 
 # Your code go above, nothing to change after this line:
 print(lyrics_generator([0,0,1,1,0,0,0]))
